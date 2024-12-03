f85c0ded78d420e5989150a0553459df893a2b88
nicoleineza <nicole.ineza@ashesi.edu.gh>
2024-12-03 15:46:39 +0000

    connection

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,6 +1,6 @@
 <?php
-define('SERVER', '169.239.251.102');  // Use the server IP
+define('SERVER', 'localhostgit ');  // Use the server IP
 define('USERNAME', 'nicole.ineza');  // Replace with your MySQL username
-define('PASSWD', 'niyitegeka18!');  // Replace with your MySQL password (after changing it)
+define('PASSWD', 'Niyitegeka18!');  // Replace with your MySQL password (after changing it)
 define('DATABASE', 'webtech_fall2024_nicole_ineza');
 ?>
