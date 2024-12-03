d27dca6037a7ffb6f605e283dc8f7e17c9eca04f
nicoleineza <nicole.ineza@ashesi.edu.gh>
2024-12-03 14:44:40 +0000

    connection

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,6 +1,6 @@
 <?php
 define('SERVER', 'localhost');  // Use the server IP
-define('USERNAME', 'root');  // Replace with your MySQL username
-define('PASSWD', '');  // Replace with your MySQL password (after changing it)
-define('DATABASE', 'imena');
+define('USERNAME', 'nicole.ineza');  // Replace with your MySQL username
+define('PASSWD', 'niyitegeka18!');  // Replace with your MySQL password (after changing it)
+define('DATABASE', 'webtech_fall2024_nicole_ineza');
 ?>
